4f5c1e8341a697423f09b4657be6ae3fb83ac1b2
ViniciusPeraro <38511977+ViniciusPeraro@users.noreply.github.com>
2018-05-09 22:55:42 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,19 @@
+//Vinicius Peraro de Oliveira 11721ECP007
+#include <stdio.h>
+int main(){
+	char str[256];
+	scanf("%s",&str);
+	int i,nada;
+	for (i=0;str[i]!='\0';i++){
+		str[i]=(int)str[i];
+		if (str[i]>=48 && str[i]<=57){
+			printf("%d",str[i]-48);
+			nada=1;
+		} else {
+			nada=0;
+		}
+	}
+	if(nada==0){
+		printf("%d",nada);
+	}
+}
