74a6032a62fe746966f004bd81aff97d576a3e58
ViniciusPeraro <38511977+ViniciusPeraro@users.noreply.github.com>
2018-05-26 23:59:47 -0300

    Add files via upload

---
+++ P7.c
@@ -0,0 +1,23 @@
+//Vinicius Peraro 11721ECP007
+#include <stdio.h>
+int A(int a,int b){
+    if (a==0){
+    	return b+1;	
+	}
+    if (a>0 && b==0){
+    	return A(a-1, 1);	
+	}
+    if (a>0 && b>0){
+    	return A(a-1,A(a,b-1));
+	}
+    return A(a-1,A(a,b-1));
+}
+int main ()
+{
+    int n=0, m=0;
+    printf("Insira o valor de M e N (nessa ordem): ");
+    scanf("%d %d",&m,&n);
+    getchar();
+    printf("%d",A(m, n));
+    return 0;
+}
