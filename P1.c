098ff6d0fc75cd28e3b6002b8f8385dee83ea0d9
ViniciusPeraro <38511977+ViniciusPeraro@users.noreply.github.com>
2018-06-22 23:48:02 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -3,32 +3,29 @@
 int main(void)
 {
     int i,st=0;
-    char bi[255];
-    scanf("%s",&bi);
+    unsigned char bi[255];
+    printf("Insira uma sequencia de '0' e '1': ");
+    scanf("%s",bi);
     for(i=0;bi[i]!='\0';i++){
-        if(st==0)
-        {
-               if(bi[i]=='0'){st = 0;}
+    	
+        if(st==0){
+        	if(bi[i]=='0'){st = 0;}
             if(bi[i]=='1'){st = 1;}
         }
-           else if(st==1)
-        {
+    	else if(st==1){
             if(bi[i]=='1'){st = 0;}
             if(bi[i]=='0'){st = 2;}
         }
-           else if(st==2)
-        {
+        else if(st==2){
             if(bi[i]=='1'){st = 2;}
             if(bi[i]=='0'){st = 1;}
-           }
+        }
     }
-    if(st==0)
-    {
-        printf("O numero inserido e multiplo de 3");
+    if(st==0){
+        printf("\nE multiplo de 3.");
     }
-    else if(st!=0)
-    {
-        printf("O numero inserido nao e multiplo de 3");
+    else if(st!=0){
+        printf("\nNao e multiplo de 3.");
     }
+    return 0;
 }
-
